9f9177a3f612ff1d3a860dc9455f1db9f1c59ad7
marvelao <104050477+marvelao@users.noreply.github.com>
2026-01-17 14:22:14 +0800

    Add ui/__init__.py

---
+++ ui/__init__.py
@@ -0,0 +1,14 @@
+"""
+UI模块
+"""
+from .main_window import MainWindow
+from .config_frame import ConfigFrame, ModelConfigFrame
+from .result_frame import ResultFrame, ModelResultFrame
+
+__all__ = [
+    'MainWindow',
+    'ConfigFrame',
+    'ModelConfigFrame',
+    'ResultFrame',
+    'ModelResultFrame'
+]
